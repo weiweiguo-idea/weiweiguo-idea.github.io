1a3b333847bbaec6df29c573297dbdaf18bdf13d
weiweiguo-idea <57023887+weiweiguo-idea@users.noreply.github.com>
2025-08-16 06:16:22 +0000

    Deploying to gh-pages from @ weiweiguo-idea/weiweiguo-idea.github.io@9c296b0be27b932f51a5f7693501e8f2f71c4a90 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-attended-the-most-esa-2025-dragon-6-symposium-and-gave-an-oral-presentation-on-our-recent-project-progress-in-qingdao",
+          title: 'Attended the MOST-ESA 2025 Dragon 6 Symposium and gave an oral presentation on...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
@@ -501,35 +501,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("mailto:%77%65%69%77%65%69%67%75%6F@%74%6F%6E%67%6A%69.%65%64%75.%63%6E", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=0vHW25wAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
